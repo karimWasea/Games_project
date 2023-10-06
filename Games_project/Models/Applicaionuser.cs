27b2abe7eaefda848c06313d7f1c08621eaab097
karimWasea <131551998+karimWasea@users.noreply.github.com>
2023-10-06 21:28:23 +0300

    Add project files.

---
+++ Games_project/Models/Applicaionuser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Games_project.Models
+{
+    public class Applicaionuser :IdentityUser
+    {
+        public DateTime? BirthDate { get; set; }
+        public string ProfileImage { get; set; }
+        public Gender Gender { get; set; }
+        public string Adress { get; set; }
+    }
+
+
+   public enum Gender
+    {
+    }
+}
---
+++ Games_project/Program.cs
@@ -0,0 +1,56 @@
+using Games_project.Models;
+
+using GameZone.Data;
+using Microsoft.AspNetCore.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+
+//builder.Services.AddDbContext<ApplicationDbContext>(options => 
+//    options.UseSqlServer(connectionString));
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                                ?? throw new InvalidOperationException("No connection string was found"); 
+IServiceCollection serviceCollection =
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+//builder.Services.AddDefaultIdentity<Applicaionuser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDBcontext>();
+
+//builder.Services.AddDefaultIdentity<Applicaionuser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDBcontext>();
+
+
+
+builder.Services.AddIdentity<Applicaionuser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
+.AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<ICategoriesService, CategoriesService>();
+builder.Services.AddScoped<IDevicesService, DevicesService>();
+builder.Services.AddScoped<IGamesService, GamesService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
